1e67a8798b09a55e2170ddc30c2e745d949c7a3e
benandante <foktay@gmail.com>
2014-01-16 11:40:57 +0100

    local

--- www/js/index.js
+++ www/js/index.js
@@ -44,7 +44,27 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
         
+        var now                  = new Date().getTime(),
+        _60_seconds_from_now = new Date(now + 6*1000);
+        window.plugin.notification.local.add({
+            id:         1, // is converted to a string
+            title:      'Reminder',
+            message:    'Dont forget to buy some flowers.',
+            repeat:     'weekly',
+            date:       _60_seconds_from_now,
+            foreground: 'foreground',
+            background: 'background'
+        });
+
        
         console.log('Received Event: ' + id);
     }
+    
+	function foreground (id) {
+	    console.log('I WAS RUNNING ID='+id)
+	}
+	
+	function background (id) {
+	    console.log('I WAS IN THE BACKGROUND ID='+id)
+	}
 };
