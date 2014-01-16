6e46d619f1da65fd88f3d3672d8eb9bc5bf515ed
benandante <foktay@gmail.com>
2014-01-16 13:27:40 +0100

    defdfdf

--- www/js/index.js
+++ www/js/index.js
@@ -43,28 +43,9 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-        navigator.notification.alert("before timer");
-        var now                  = new Date().getTime(),
-        _60_seconds_from_now = new Date(now + 6*1000);
-        window.plugin.notification.local.add({
-            id:         1, // is converted to a string
-            title:      'Reminder',
-            message:    'Dont forget to buy some flowers.',
-            repeat:     'weekly',
-            date:       _60_seconds_from_now,
-            foreground: 'foreground',
-            background: 'background'
-        });
-
-        navigator.notification.alert("after timer");
+       
         console.log('Received Event: ' + id);
     }
     
-	function foreground (id) {
-	    console.log('I WAS RUNNING ID='+id)
-	}
-	
-	function background (id) {
-	    console.log('I WAS IN THE BACKGROUND ID='+id)
-	}
+
 };
