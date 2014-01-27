f5c2defad03f6c78501c40e64646ce42378b6224
benandante <foktay@gmail.com>
2014-01-27 10:30:34 +0100

    aaaaa

--- www/js/index.js
+++ www/js/index.js
@@ -28,8 +28,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.addEventListener('online', this.onOnline, false);
-        document.addEventListener('offline', this.onOffline, false);
+      
     },
     // deviceready Event Handler
     //
